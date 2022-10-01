a7a8481a5f95898dd4e0819d094cad27495b981b
hector.gv00 <hector.gv00@gmail.com>
2022-10-01 19:37:01 +0200

    Acabada la carta interactiva

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,47 @@
-// 
+var entrantes = document.getElementById("entrantes");
+var primeros = document.getElementById("primeros");
+var segundos = document.getElementById("segundos");
+var postres = document.getElementById("postres");
+
+function Entrantes(){
+
+
+    if( entrantes.classList.value == "container mt-3 mb-3 noDisplay"){
+        entrantes.classList.remove("noDisplay")
+        primeros.classList.add("noDisplay")
+        segundos.classList.add("noDisplay")
+        postres.classList.add("noDisplay")        
+    }
+}
+
+function Primeros(){
+
+
+    if( primeros.classList.value == "container mt-3 mb-3 noDisplay"){
+        primeros.classList.remove("noDisplay")
+        entrantes.classList.add("noDisplay")
+        segundos.classList.add("noDisplay")
+        postres.classList.add("noDisplay")        
+    }
+}
+
+function Segundos(){
+
+    if( segundos.classList.value == "container mt-3 mb-3 noDisplay"){
+        segundos.classList.remove("noDisplay")
+        primeros.classList.add("noDisplay")
+        entrantes.classList.add("noDisplay")
+        postres.classList.add("noDisplay")        
+    }
+}
+
+function Postres(){
+
+    if( postres.classList.value == "container mt-3 mb-3 noDisplay"){
+        postres.classList.remove("noDisplay")
+        primeros.classList.add("noDisplay")
+        entrantes.classList.add("noDisplay")
+        segundos.classList.add("noDisplay")        
+    }
+}
+
